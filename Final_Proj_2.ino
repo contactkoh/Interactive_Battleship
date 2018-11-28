c863100ee710dd3b1a2dab7b2bcbf824ffdaf9a0
Chris Brownell <42624201+chrisbrownell@users.noreply.github.com>
2018-11-28 18:54:46 -0500

    Add files via upload

--- Final_Proj_2.ino
+++ Final_Proj_2.ino
@@ -1,9 +1,17 @@
 #include <Servo.h>
+#include <Adafruit_NeoPixel.h>
+
+#define NUM_LEDS 3
+#define BRIGHTNESS 100
+#define PIN 6
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
 Servo tiltServo;  // create servo object to control a servo
 Servo panServo;
 
 int i = 0;
+int recoil = 5;
 byte x_pos[] = {
    45,  41,  36,  31,  27,  19,  15,  10,   5,   2,
     0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
@@ -45,6 +53,9 @@ byte hits[] = {
 
 void setup() {
   pinMode(13, OUTPUT); // for LED status
+  strip.setBrightness(BRIGHTNESS);
+  strip.begin();
+  strip.show();
   tiltServo.attach(10);  // attaches the servo on pin 9 to the servo object
   panServo.attach(9);
   Serial.begin(9600);
@@ -65,10 +76,18 @@ void loop() {
         tiltServo.write(y_pos[i]);    // tell tilt servo to go to position A0 on the board
         panServo.write(x_pos[i]);
         Serial.println("Y: " + String(y_pos[i]) + " X: " + String(x_pos[i]));
+        delay(500);
+        tiltServo.write(y_pos[i] - recoil);  
+        delay(500);
+        tiltServo.write(y_pos[i]);
           if(hits[i]){
             digitalWrite(13,HIGH);
+            strip.setPixelColor(i, strip.Color(255, 0, 0));
+            strip.setPixelColor(i+1, strip.Color(255, 0, 0));
+            strip.setPixelColor(i+2, strip.Color(255, 0, 0));
+            strip.show();
           }
-        delay(3000);
+        delay(1000);
         tiltServo.write(95);    
         panServo.write(20);
         delay(1000);

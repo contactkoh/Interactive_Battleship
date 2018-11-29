c83d269d23fad4cf358a2914b4e329d3c6dedc8e
contactkoh <39782955+contactkoh@users.noreply.github.com>
2018-11-28 20:18:40 -0500

    Add files via upload

--- Final_Proj_2.ino
+++ Final_Proj_2.ino
@@ -1,3 +1,29 @@
+/*
+ * MP3 
+ */
+
+#include <SoftwareSerial.h>
+#define ARDUINO_RX 5//should connect to TX of the Serial MP3 Player module  (b/c pin 5 = Arduino's RX)
+#define ARDUINO_TX 6//connect to RX of the module  (b/c pin 6 = Arduino's TX)
+SoftwareSerial mySerial(ARDUINO_RX, ARDUINO_TX);
+static int8_t Send_buf[8] = {0} ;
+
+#define CMD_SEL_DEV 0X09
+#define DEV_TF 0X02
+#define CMD_PLAY_W_VOL 0X22 //22
+#define CMD_PLAY 0X0D
+#define CMD_PAUSE 0X0E
+#define CMD_PREVIOUS 0X02
+#define CMD_NEXT 0X01
+#define CMD_RAND 0X03
+
+
+
+
+/*
+ * Bluetooth
+ */
+
 #include <Servo.h>
 #include <Adafruit_NeoPixel.h>
 
@@ -52,6 +78,19 @@ byte hits[] = {
 };
 
 void setup() {
+
+  /*
+   * Mp3
+   */
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  delay(500);//Wait chip initialization is complete
+  sendCommand(CMD_SEL_DEV, DEV_TF);//select the TF card  
+  delay(200);//wait for 200ms
+  sendCommand(CMD_PLAY_W_VOL, 0X1E01);//play the first song with volume 30 class
+  Serial.println("Setting Up initially...");
+
+  
   pinMode(13, OUTPUT); // for LED status
   strip.setBrightness(BRIGHTNESS);
   strip.begin();
@@ -77,8 +116,22 @@ void loop() {
         panServo.write(x_pos[i]);
         Serial.println("Y: " + String(y_pos[i]) + " X: " + String(x_pos[i]));
         delay(500);
-        tiltServo.write(y_pos[i] - recoil);  
-        delay(500);
+        
+        /*
+         * MP3
+         */
+        sendCommand(CMD_PLAY_W_VOL, 0X1E03);//play the first song with volume 30 class
+        delay(2000);
+        // turn LED on:
+        //Serial.println("If Button STate HIGH" + buttonState);
+        //digitalWrite(ledPin, HIGH);
+        sendCommand(CMD_PLAY_W_VOL, 0X1E02);//play the first song with volume 30 class
+        delay(2000);
+        sendCommand(CMD_PLAY_W_VOL, 0X1E01);//play the first song with volume 30 class
+        
+        
+        //tiltServo.write(y_pos[i] - recoil);  
+        //delay(500);
         tiltServo.write(y_pos[i]);
           if(hits[i]){
             digitalWrite(13,HIGH);
@@ -117,6 +170,25 @@ void loop() {
       Serial.println("RESET");
       digitalWrite(13,LOW);
     }*/
-   
-    
+      
+}
+
+
+void sendCommand(int8_t command, int16_t dat)
+{
+  delay(20);
+  Serial.println("Inside sendCommand");
+
+  Send_buf[0] = 0x7e; //starting byte
+  Send_buf[1] = 0xff; //version
+  Send_buf[2] = 0x06; //the number of bytes of the command without starting byte and ending byte
+  Send_buf[3] = command; //
+  Send_buf[4] = 0x00;//0x00 = no feedback, 0x01 = feedback
+  Send_buf[5] = (int8_t)(dat >> 8);//datah
+  Send_buf[6] = (int8_t)(dat); //datal
+  Send_buf[7] = 0xef; //ending byte
+  for(uint8_t i=0; i<8; i++)//
+  {
+    mySerial.write(Send_buf[i]) ;
+  }
 }
